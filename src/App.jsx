0ce3a5893c95d02040a812e32cd3984701ca993a
Thomas Sudweeks <thomassu@byu.edu>
2025-01-09 22:42:05 -0700

    React implemented

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1>Welcome!</h1>
+      <h2>This is Thomas' website</h2>
+    </>
+  )
+}
+
+export default App
