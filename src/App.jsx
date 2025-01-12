133326bf602c50a6291408453d7827cf36ae5638
Thomas Sudweeks <thomassu@byu.edu>
2025-01-11 23:59:12 -0700

    add pentagon logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import { useState } from 'react'
 import './App.css'
+import gradient from './../resources/blur_gradient.png'
+import pentagon from './../resources/pentagon.png'
 
 function App() {
   return (
     <>
-      <h1>Welcome!</h1>
-      <h2>This is Thomas' website</h2>
+      <a>
+        <img src={gradient} className='image' />
+        <img src={pentagon} className='pentagon' />
+      </a>
+      <div className='bg upper'></div>
+      <div className='bg left'></div>
+      <div className='bg right'></div>
+      <div className='bg lower'></div>
+      <h1 className='title'>Welcome!</h1>
+      <h3>This is Thomas' website</h3>
     </>
   )
 }
