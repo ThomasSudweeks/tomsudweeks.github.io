51502fdd8c09db3acbf34063be1e97411d64ae87
Thomas Sudweeks <thomassu@byu.edu>
2025-01-12 01:25:06 -0700

    scroll fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,18 @@
 import { useState } from 'react'
 import './App.css'
-import gradient from './../resources/blur_gradient.png'
-import pentagon from './../resources/pentagon.png'
+import gradient from './../resources/radial_gradient.png'
+import overlay from './../resources/shadow_overlay.png'
 
 function App() {
   return (
     <>
       <a>
         <img src={gradient} className='image' />
-        <img src={pentagon} className='pentagon' />
+        <img src={overlay} className='image overlay' />
       </a>
-      <div className='bg upper'></div>
-      <div className='bg left'></div>
-      <div className='bg right'></div>
-      <div className='bg lower'></div>
       <h1 className='title'>Welcome!</h1>
       <h3>This is Thomas' website</h3>
+      <p>That's it. Thanks for stopping by :)</p>
     </>
   )
 }
