27b7b4d17a5a89aa03eca8f07ea33b3cd0136167
Thomas Sudweeks <thomassu@byu.edu>
2025-02-23 15:12:05 -0700

    Running gh-paging from docs folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -10,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="pombuds" element={<Pom />} />
+        <Route path="other" element={<Pom />} />
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
